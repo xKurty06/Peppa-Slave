603481ea16288cdbf070bf81e6f9ffc3b8ae56fd
xKurty06 <zeankurtbalboa06@gmail.com>
2023-01-13 23:38:38 +0800

    fixed dm cmd

--- commands/general/dm.js
+++ commands/general/dm.js
@@ -13,10 +13,12 @@ module.exports = {
 	code: `
 $sendDM[> $get[msg];$get[user];no]
 $memberExists[$get[user];{newEmbed:{title:$get[error] | I couldn't find the user.}{color:$get[clr2]}}]
+$onlyIf[$get[user]!=;{newEmbed:{title:$get[error] | I couldn't find the user.}{color:$get[clr2]}}]
+
 $let[user;$findUser[$message[1];no]]
 $let[msg;$messageSlice[1]]
 
-$argsCheck[>1;{newEmbed:{author:║ Wrong Arguments Given!:$userAvatar}{field:$get[error] Command's Usage#COLON#:\`\`\`js
+$argsCheck[>1;{newEmbed:{author:║ Wrong Arguments Given!:$userAvatar[$authorID]}{field:$get[error] Command's Usage#COLON#:\`\`\`js
 Aliases: #RIGHT#$get[alias]#LEFT#\n~> Usage:\n$get[usage]\n\n~> Example:\n$get[example]\`\`\`}{footer:$get[params]}{color:$get[clr2]}}]
 $cooldown[$get[cd];{newEmbed:{description:$get[error] | Hey! Slow down, you have to wait until \` %sec%.%ms%s \` before running this command again.}{color:$get[clr2]}}]
 $deletecommand
