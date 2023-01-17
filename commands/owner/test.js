d42be687cd60a6ed72d433713a3f73cefd277a17
xKurty06 <zeankurtbalboa06@gmail.com>
2023-01-17 23:53:46 +0800

    fixed vouch system: 1/17/23

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -3,6 +3,9 @@ module.exports = {
 	code: `
 $channelSendMessage[$channelID;{newEmbed:{title:<#COLON#peppapigshoppe_happypeppa#COLON#1060268959724740720> Hello Shopper!}{description:> <a#COLON#peppapigshoppe_arrow#COLON#1059035766115815466> | Want to order or report now? Click the Green Button below to make an <:peppapigshoppe:1057625427029540904>Order Form and Red Button for the <:peppapigshoppe_report:1061638968195690577>Report Form!\n\n> 📌 | If the bot is offline, please send the format manually from <#1059307766684073984> channel.}{color:$get[clr]}}{actionRow:{button:Order here:success:orderForm:no:<#COLON#peppapigshoppe#COLON#1057625427029540904>}{button:Nitro:danger:nitroReportForm:no:<#COLON#peppapigshoppe_report#COLON#1061638968195690577>}{button:Premium Acc:danger:premReportForm:no:<#COLON#peppapigshoppe_report#COLON#1061638968195690577>}}]
 
+$onlyForIDs[$joinSplitText[;];]
+$textSplit[$getVar[botOwners;bot];/]
+
 $let[arrow;<a#COLON#peppapigshoppe_arrow#COLON#1059035766115815466>]
 $let[error;❌]
 $let[check;✅]
--- commands/setup/vouchSystem/vouchSystem.js
+++ commands/setup/vouchSystem/vouchSystem.js
@@ -4,9 +4,9 @@ module.exports = {
 <:peppapig:1057625427029540904> ***Thank you for vouching! your warranty has been activated.*** <:peppapig:1057625427029540904>
 $reply[$messageID;yes]
 
-$onlyIf[$get[attch]!=;{newEmbed:{title:$get[error] | Wrong format given!}{description:*Please provide a screenshot of your purchase!*}{color:$get[clr2]}}{delete:5s}]
+$onlyIf[$get[attch]!=;<@$authorID>{newEmbed:{title:$get[error] | Wrong format given!}{description:*Please provide a screenshot of your purchase!*}{color:$get[clr2]}}{extraOptions:{delete:15s}}]
 
-$onlyIf[$checkContains[$message;vouch;Vouch]==true;{newEmbed:{title:$get[error] | Wrong format given!}{description:**Proper Format:** *\` - vouch @$userTag[886620575735373825] for 1 pc of Nitro Boost, tysm! \`*}{color:$get[clr2]}}{extraOptions:{delete:3s}}{reply:{messageReference:$messageID}}]
+$onlyIf[$checkContains[$message;vouch;Vouch]==true;<@$authorID>{newEmbed:{title:$get[error] | Wrong format given!}{description:**Proper Format:** *\` - vouch @$userTag[886620575735373825] for 1 pc of Nitro Boost, tysm! \`*}{color:$get[clr2]}}{extraOptions:{delete:15s}}]
 $let[attch;$messageAttachment]
 $onlyForChannels[$get[vChannel];]
 $onlyIf[$serverChannelExists[$get[vChannel]]==true;]
